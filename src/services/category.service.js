dbe9f8dc5c69b8c444c55933a826df189045f0a4
get0r <jahsirtg@protonmail.com>
2022-05-01 11:06:19 +0300

    category.service -- add pagination & query

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,5 +1,7 @@
 const CategoryModel = require('../database/models/business.model');
 
+const PAGE_SIZE = 10;
+
 const create = async (catInfo) => {
   const newCat = new CategoryModel(catInfo);
 
@@ -8,8 +10,13 @@ const create = async (catInfo) => {
   return savedCat;
 };
 
-const getAll = async () => {
-  const categories = await CategoryModel.find({ }).lean();
+const getAll = async (query = {}, sort = {}, page = 1) => {
+  const skip = (page - 1) * PAGE_SIZE;
+  const categories = await CategoryModel.find(query)
+    .skip(skip)
+    .sort(sort)
+    .limit(PAGE_SIZE)
+    .lean();
 
   return categories;
 };
