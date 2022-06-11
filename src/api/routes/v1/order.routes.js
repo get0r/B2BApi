7095914ecdb2adb4d60fb70b69e21753e1f70f12
get0r <jahsirtg@protonmail.com>
2022-06-11 14:44:13 +0300

    integrate order creation with route

--- src/api/routes/v1/order.routes.js
+++ src/api/routes/v1/order.routes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 
-// const AuthController = require('../../controller/auth.controller');
+const OrderController = require('../../controller/order.controller');
 // const AuthValidator = require('../../middlewares/validation/auth.validator');
 
 const { ORDER_ROUTES, ROUTES, withId } = require('../../../helpers/constants/route.constants');
+const { authUser } = require('../../middlewares/auth/auth.middleware');
 // const { validateAsync } = require('../../middlewares/validation/joi.validator');
 
 const orderRouter = express.Router();
 
 orderRouter
-  .post(ROUTES.ROOT, (req, res) => res.send(`${req.url} working!`));
+  .post(ROUTES.ROOT, authUser, OrderController.create);
 
 orderRouter
   .put(withId(ROUTES.ROOT, 'oId'), (req, res) => res.send(`${req.url} working!`));
